69794b0904bfac5e50216556fd5475918c9bf477
Pratz-20 <2020bec114@sggs.ac.in>
2025-01-26 01:44:13 +0530

    create new component

---
+++ src/app/pratik/pratik.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pratik',
+  imports: [],
+  templateUrl: './pratik.component.html',
+  styleUrl: './pratik.component.css'
+})
+export class PratikComponent {
+
+}
