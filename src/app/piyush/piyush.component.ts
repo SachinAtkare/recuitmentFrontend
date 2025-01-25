f9ed5b743b7dc3136fe06f613fc515d5c79a4ab7
piyush28329289 <piyushup158@gmail.com>
2025-01-26 02:19:03 +0530

    i have submitted my component

---
+++ src/app/piyush/piyush.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-piyush',
+  imports: [],
+  templateUrl: './piyush.component.html',
+  styleUrl: './piyush.component.css'
+})
+export class PiyushComponent {
+
+}
